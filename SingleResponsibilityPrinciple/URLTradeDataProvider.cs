e7a5f5c1b43e6ade33259174a258ad9abfa4e8e7
thauck <thauck@css.edu>
2017-11-11 21:22:49 -0600

    Request 407 - created new URLTradeDataProvider class

--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
-    public class StreamTradeDataProvider : ITradeDataProvider
+    public class URLTradeDataProvider : ITradeDataProvider
     {
         public IEnumerable<string> GetTradeData()
         {
             var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
             using (var reader = new StreamReader(stream))
             {
                 string line;
@@ -20,7 +26,5 @@ namespace SingleResponsibilityPrinciple
             }
             return tradeData;
         }
-
-        private readonly Stream stream;
     }
 }

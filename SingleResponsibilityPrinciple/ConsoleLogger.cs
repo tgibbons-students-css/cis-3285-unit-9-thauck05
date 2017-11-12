a296b2100b30cf50a4ae7bed0bd3d0dda00c0c7e
thauck <thauck@css.edu>
2017-11-11 20:31:50 -0600

    Request  405 - added logging to XML log file.

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,19 +1,27 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
     public class ConsoleLogger : ILogger
     {
-        public void LogWarning(string message, params object[] args)
+        void ILogger.LogWarning(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("WARN: ", message), args);
+            Console.WriteLine(message, args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><message>" + message + "</message></log> ", args);
+            }
         }
 
-        public void LogInfo(string message, params object[] args)
+        void ILogger.LogInfo(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("INFO: ", message), args);
-        }
+            Console.WriteLine(message, args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><message>" + message + "</message></log> ", args);
+            }
+        }        
     }
 }
--- SingleResponsibilityPrinciple/SimpleTradeValidator.cs
+++ SingleResponsibilityPrinciple/SimpleTradeValidator.cs
@@ -35,13 +35,13 @@ namespace SingleResponsibilityPrinciple
 
             if (tradeAmount < 1000)
             {
-                logger.LogWarning("WARN", "Trade amount on line {0} is too small: '{1}'", tradeData[1]);
+                logger.LogWarning("Trade amount too low: '{0}'", tradeData[1]);
                 return false;
             }
 
-            if (tradeAmount > 100000)
+            if (tradeAmount >= 100001)
             {
-                logger.LogWarning("WARN", "Trade amount on line {0} is too large: '{1}'", tradeData[1]);
+                logger.LogWarning("Trade amount is too high: '{0}'", tradeData[1]);
                 return false;
             }
 

7c79e3b4c86f66d7777374b3c08e00dfe99d287a
thauck <thauck@css.edu>
2017-11-11 19:40:46 -0600

    Request 403 - add lower and upper trade data bounds.

--- SingleResponsibilityPrinciple/SimpleTradeValidator.cs
+++ SingleResponsibilityPrinciple/SimpleTradeValidator.cs
@@ -33,6 +33,18 @@ namespace SingleResponsibilityPrinciple
                 return false;
             }
 
+            if (tradeAmount < 1000)
+            {
+                logger.LogWarning("WARN", "Trade amount on line {0} is too small: '{1}'", tradeData[1]);
+                return false;
+            }
+
+            if (tradeAmount > 100000)
+            {
+                logger.LogWarning("WARN", "Trade amount on line {0} is too large: '{1}'", tradeData[1]);
+                return false;
+            }
+
             decimal tradePrice;
             if (!decimal.TryParse(tradeData[2], out tradePrice))
             {
